2cb9856120743c38381837efbe27be80139f9c1e
baronmdw <58744855+baronmdw@users.noreply.github.com>
2022-10-18 20:54:42 +0200

    Client Side fetching data from weather and post ok

--- website/app.js
+++ website/app.js
@@ -1,6 +1,9 @@
 const baseURL = "http://localhost:8080/"
 const apiKey = "&appid=ea53dd34102b8c99830e720e490422ba"
 const weatherURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+let unitStyle = "imperial";
+let country = "us";
+let zipCode = "10015";
 
 console.log("Hello World");
 const postData = async (url="", data={}) => {
@@ -24,17 +27,36 @@ const getData = async (url="") => {
 
     try {
         const retreivedData = await response.json();
-        console.log(retreivedData);
+        return retreivedData;
     } catch(error) {
         console.log("Oops, that didn't quite go well. Error: ",error);
     };
 };
 
 function getWeather() {
-    console.log("Kuckuck");
-    getData(weatherURL+"81241,DE"+apiKey);
+    //TODO: Catch empty values
+    const zipCode = document.getElementById("zipCode").value;
+    const currentMood = document.getElementById("currentMood").value;
+    let d = new Date();
+    let newDate = d.getDate()+'.'+ d.getMonth()+'.'+ d.getFullYear();
+    const weatherData = getData(weatherURL+zipCode+","+country+apiKey+"&units="+unitStyle)
+    .then(function(weatherData){
+        postData(baseURL+"addDay",{date:newDate,weather:weatherData,mood:currentMood})
+    });
+}
+
+function adaptMetric() {
+    unitStyle = document.getElementById("unitSelector").value;
+    console.log(unitStyle);
+}
+
+function adaptCountry() {
+    country = document.getElementById("countrySelector").value;
+    console.log(country);
 }
 
 postData(baseURL+"addDay",{name: "Marc"});
 getData(baseURL+"getData");
-document.getElementById("hitMe").addEventListener("click",getWeather);
+document.getElementById("submitMood").addEventListener("click",getWeather);
+document.getElementById("unitSelector").addEventListener("change",adaptMetric);
+document.getElementById("countrySelector").addEventListener("change",adaptCountry);
